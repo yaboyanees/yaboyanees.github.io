671e1167d29e9b9192e1494155f48855a7ae4f9f
Anees <yaboyanees@users.noreply.github.com>
2019-03-19 21:12:09 -0400

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,7 +2,6 @@
 	Miniport by HTML5 UP
 	html5up.net | @n33co
 	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
-*/
 
 (function($) {
 
@@ -61,3 +60,4 @@
 	});
 
 })(jQuery);
+*/
